9b970f5b3b2a8f032ffd59f2429e2a7975264de0
Skullfiredevil <jotipinto@gmail.com>
2023-09-21 12:03:29 +0100

    added a way to choose gamemode and calculations work properly

--- CodM helper/ContentView.swift
+++ CodM helper/ContentView.swift
@@ -6,46 +6,57 @@
 //
 
 import SwiftUI
+        struct ContentView: View {
+            @State private var selectedGameMode = 0
+            @State private var currentPoints = 0
+            @State private var killsNeeded = 0
 
-struct ContentView: View {
-    @State private var pointsNeeded = 0
-    @State private var pointsPerKill = 20
-    @State private var currentPoints = 0
-    @State private var killsNeeded = 0
+            let gameModes: [GameMode] = [
+                GameMode(name: "Blackout War", pointsNeeded: 1469, pointsPerKill: 20),
+                // Add more game modes here as needed
+            ]
 
-    var body: some View {
-        VStack {
-            Text("Kills Needed to Reach 1st Place")
-                .font(.title)
-                .padding()
+            var body: some View {
+                VStack {
+                    Text("Kills Needed to Reach 1st Place")
+                        .font(.title)
+                        .padding()
 
-            TextField("Points Needed for 1st Place", value: $pointsNeeded, formatter: NumberFormatter())
-                .padding()
+                    Picker("Game Mode", selection: $selectedGameMode) {
+                        ForEach(0..<gameModes.count, id: \.self) { index in
+                            Text(gameModes[index].name)
+                        }
+                    }
+                    .pickerStyle(.menu)
+                    .padding()
 
-            TextField("Points per Kill", value: $pointsPerKill, formatter: NumberFormatter())
-                .padding()
+                    TextField("Current Points", value: $currentPoints, formatter: NumberFormatter())
+                        .padding()
 
-            TextField("Current Points", value: $currentPoints, formatter: NumberFormatter())
-                .padding()
+                    HStack {
+                        Text("Total Kills Needed:")
+                        Text("\(killsNeeded)")
+                            .font(.headline)
+                    }
+                    .padding()
 
-            HStack {
-                Text("Total Kills Needed:")
-                Text("\(killsNeeded)")
-                    .font(.headline)
+                    Button("Calculate") {
+                        calculateKillsNeeded()
+                    }
+                    .padding()
+                }
+                .frame(width: 300, height: 300)
             }
-            .padding()
 
-            Button("Calculate") {
-                calculateKillsNeeded()
+            func calculateKillsNeeded() {
+                let selectedMode = gameModes[selectedGameMode]
+                let pointsDifference = max(selectedMode.pointsNeeded - currentPoints, 0)
+                killsNeeded = (pointsDifference + selectedMode.pointsPerKill - 1) / selectedMode.pointsPerKill
             }
-            .padding()
         }
-        .frame(width: 300, height: 300)
-    }
 
-    func calculateKillsNeeded() {
-        let pointsDifference = pointsNeeded - currentPoints
-        killsNeeded = pointsDifference / pointsPerKill
-    }
+struct GameMode {
+    var name: String
+    var pointsNeeded: Int
+    var pointsPerKill: Int
 }
-

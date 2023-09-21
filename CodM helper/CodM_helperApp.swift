609809aa6b5cb47a32b16135d44fee83176319bf
Skullfiredevil <jotipinto@gmail.com>
2023-09-21 11:14:30 +0100

    Initial Commit

---
+++ CodM helper/CodM_helperApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodM_helperApp.swift
+//  CodM helper
+//
+//  Created by Tiago Pinto on 21/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct CodM_helperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
